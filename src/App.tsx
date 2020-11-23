1613a0dd7fa9e92a6c6e4906753dce8a5f53f883
khizarkhan07 <khizar062@gmail.com>
2020-11-23 12:38:28 +0500

    create customer drawer

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import './App.css';
 import { Switch, Route, withRouter, RouteComponentProps, Link } from 'react-router-dom';
 import HomePage from "./pages/HomePage";
 import EditCustomer from "./pages/customer/EditCustomer";
-import CreateCustomer from "./pages/customer/CreateCustomer";
 import CreateSale from "./pages/sales/CreateSale";
 import Sales from "./pages/sales/Sales";
 import Navbar from "./pages/Navbar";
@@ -16,9 +15,7 @@ const App: React.FC<RouteComponentProps<any>>  = () => {
         <Switch>
           <Route path={'/'} exact component={HomePage} />
           <Route path={'/sales'} exact component={Sales} />
-          <Route path={'/create/customer'} exact component={CreateCustomer} />
           <Route path={'/create/sale'} exact component={CreateSale} />
-          <Route path={'/create'} exact component={CreateCustomer} />
           <Route path={'/edit/:id'} exact component={EditCustomer} />
 
         </Switch>
